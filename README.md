6338b639253f93fb17b5ca3fef89c363c2536de1
Andrea Quirini <A.Quirini@almaviva.it>
2024-06-28 15:07:18 +0200

    cambio nome

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Ninja.Sharp.OpenDb2
+
 An open source client for integration with IBM DB2
+
+## Usage
+
+On Windows:
+
+``` csharp
+builder.services
+   .AddWinDb2Services(connectionString, configuration);
+```
+
+On Linux:
+
+``` csharp
+builder.services
+   .AddLnxDb2Services(connectionString, configuration);
+```
--- Services/ServiceRegistration.cs
+++ Services/ServiceRegistration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using OpenDb2.Drivers.Linux;
 using OpenDb2.Drivers.Windows;
 using OpenDb2.Interfaces.Linux;
@@ -8,15 +9,25 @@ namespace OpenDb2.Services
 {
     public static class ServiceRegistration
     {
-        public static IServiceCollection AddWinDb2Services(this IServiceCollection services, string connectionString)
+        public static IServiceCollection AddWinDb2Services(this IServiceCollection services, string connectionString, IConfiguration config)
         {
+            ConfigurationBuilder builder = new();
+
+            builder.AddConfiguration(config);
+            builder.Build();
+
             services.AddScoped<IWinDb2Connection>(sp => new WinDb2Connection(connectionString));
 
             return services;
         }
 
-        public static IServiceCollection AddLnxDb2Services(this IServiceCollection services, string connectionString)
+        public static IServiceCollection AddLnxDb2Services(this IServiceCollection services, string connectionString, IConfiguration config)
         {
+            ConfigurationBuilder builder = new();
+
+            builder.AddConfiguration(config);
+            builder.Build();
+
             services.AddScoped<ILnxDb2Connection>(sp => new LnxDb2Connection(connectionString));
 
             return services;
