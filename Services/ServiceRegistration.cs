e49cafe4403d21742976dbc5cb44e79811d9ef3a
Francesco Del Re <francesco.delre@protonmail.com>
2024-07-14 18:03:47 +0200

    Add generic AddDb2Services registration

--- Services/ServiceRegistration.cs
+++ Services/ServiceRegistration.cs
@@ -3,15 +3,54 @@
 
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ninja.Sharp.OpenDb2.Utilities;
 using OpenDb2.Drivers.Linux;
 using OpenDb2.Drivers.Windows;
 using OpenDb2.Interfaces.Linux;
 using OpenDb2.Interfaces.Windows;
+using System.Runtime.InteropServices;
 
 namespace OpenDb2.Services
 {
     public static class ServiceRegistration
     {
+        /// <summary>
+        /// Registers the Db2 services based on the current operating system platform.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="connectionString"></param>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        /// <exception cref="NotSupportedException"></exception>
+        public static IServiceCollection AddDb2Services(this IServiceCollection services, string connectionString, IConfiguration config)
+        {
+            ConfigurationBuilder builder = new();
+
+            builder.AddConfiguration(config);
+            builder.Build();
+
+            switch (EnvironmentDetector.GetCurrentOSPlatform())
+            {
+                case var os when os == OSPlatform.Windows:
+                    services.AddScoped<IWinDb2Connection>(sp => new WinDb2Connection(connectionString));
+                    break;
+                case var os when os == OSPlatform.Linux:
+                    services.AddScoped<ILnxDb2Connection>(sp => new LnxDb2Connection(connectionString));
+                    break;
+                default:
+                    throw new NotSupportedException("Unsupported operating system platform.");
+            }
+
+            return services;
+        }
+
+        /// <summary>
+        /// Registers the Db2 services for Windows environment.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="connectionString"></param>
+        /// <param name="config"></param>
+        /// <returns></returns>
         public static IServiceCollection AddWinDb2Services(this IServiceCollection services, string connectionString, IConfiguration config)
         {
             ConfigurationBuilder builder = new();
@@ -24,6 +63,13 @@ namespace OpenDb2.Services
             return services;
         }
 
+        /// <summary>
+        /// Registers the Db2 services for Linux environment.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="connectionString"></param>
+        /// <param name="config"></param>
+        /// <returns></returns>
         public static IServiceCollection AddLnxDb2Services(this IServiceCollection services, string connectionString, IConfiguration config)
         {
             ConfigurationBuilder builder = new();
---
+++ Utilities/EnvironmentDetector.cs
@@ -0,0 +1,28 @@
+﻿// (c) 2024 thesharpninjas
+// This code is licensed under MIT license (see LICENSE.txt for details)
+
+using System.Runtime.InteropServices;
+
+namespace Ninja.Sharp.OpenDb2.Utilities
+{
+    public class EnvironmentDetector
+    {
+        public static OSPlatform GetCurrentOSPlatform()
+        {
+            return RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? OSPlatform.Windows :
+                   RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? OSPlatform.Linux :
+                   RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? OSPlatform.OSX :
+                   OSPlatform.Create("Other");
+        }
+
+        public static bool IsWindows()
+        {
+            return RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        }
+
+        public static bool IsLinux()
+        {
+            return RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+        }
+    }
+}
